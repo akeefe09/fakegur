c0dbd4b33dd99478649a122e01f864a340cc58fc
Alexander Keefe <akeefe09@yahoo.com>
2018-05-14 10:07:48 -0400

    Add post components

---
+++ frontend/components/posts/post_info.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+class PostInfo extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  socialIcons() {
+    return (
+      <div className="post-info-icons">
+        
+      </div>
+    )
+  }
+};
