8ce19aa1ede13644c3983392ad7e0d31074edb8d
Alexander Keefe <akeefe09@yahoo.com>
2018-05-13 23:43:17 -0400

    fix syntax errors

--- frontend/actions/post_actions.js
+++ frontend/actions/post_actions.js
@@ -1,6 +1,12 @@
 import * as PostUtil from '../util/post_api_util';
 
-export const receiveAllPosts = (posts) => ({
-  type: RECEIVE_ALL_POSTS,
-  posts,
+export const receiveAllPosts = (posts) => {
+  return (
+    type: RECEIVE_ALL_POSTS,
+    posts,
+  )
+};
+
+export const receivePost = (post) => ({
+
 });
