9592decce79b063aca7370afc2e53b0ff40afe58
Alexander Keefe <akeefe09@yahoo.com>
2018-05-17 10:17:19 -0400

    Add comment container

--- frontend/components/comments/comment_container.jsx
+++ frontend/components/comments/comment_container.jsx
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux';
 
-const mapStateToProps =
+const mapStateToProps = (state, ownProps) => {
+  return({
+    currentComment: state.entities.comments[ownProps.id];
+    currentUser: state.session.currentUser,
+    state.entities.posts[]
+    author: state.entities.users[currentComment.user_id]
+  });
+}
 
 
 const mapDispatchToProps = (dispatch) => {
   return({
     requestComment: id => dispatch(requestComment(id))
-  })
+  });
 }
 
 export default connect(mapDispatchToProps, mapStateToProps)(Comment);
