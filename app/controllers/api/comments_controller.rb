eba5081a34bcdb84f4d6a756279696ef09053fde
Alexander Keefe <akeefe09@yahoo.com>
2018-05-17 17:55:18 -0400

    Remove debugger

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,7 +2,6 @@ class Api::CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.new(comment_params)
-    debugger
     if @comment.save
       render :show
     end
