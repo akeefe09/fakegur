670e148417806a45c16cce8de53345ee5569db72
Alexander Keefe <akeefe09@yahoo.com>
2018-05-17 12:48:49 -0400

    Add post styling

--- frontend/components/navbar/menu_left_dropdown_items.jsx
+++ frontend/components/navbar/menu_left_dropdown_items.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const LeftDropDown = ({ clearDropdowns }) => (
   <div className="dropdown-list" onClick={(event) => event.stopPropagation()} >
     <a href='https://github.com/akeefe09/fakegur/blob/master/README.md' target="_blank">about</a>
-    <a href='https://github.com/akeefe09' target="_blank">my github</a>
-    <a href='https://www.linkedin.com/in/alexander-keefe-785551130/' target="_blank">my linkedin</a>
+    <a href='https://github.com/akeefe09' target="_blank">github</a>
+    <a href='https://www.linkedin.com/in/alexander-keefe-785551130/' target="_blank">linkedin</a>
   </div>
 );
 
--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -13,33 +13,98 @@ class Post extends React.Component {
     const post = this.props.post;
     return (
       <div className="post-div">
+        <header className='navbar-container'>
+          <NavBar/>
+        </header>
         {post ? (
-          <div>
-            <header className='navbar-container'>
-              <NavBar/>
-            </header>
-                <div className="post-title">{post.title}</div>
+        <div className="whole-container">
+          <div className="post-container-main">
+            <div className="post-box">
+              <div className="post-header">
+                <div className="title-container">
+                    <div className="post-title">{post.title}</div>
 
-                <div className="post-user">
-                  <Link to={`/users/${post.user_id}`}>
-                    {post.user_id}
-                  </Link>
+                    <div className="post-user">
+                      <span className="by">by</span>
+                      <Link to={`/users/${post.user_id}`}>
+                        {post.user_id}
+                      </Link>
+                    </div>
+                  </div>
                 </div>
 
                 <div className="post-image">
-                  <img src={post.image_url} />
+                  <div className="post-image-container">
+                    <div className="post-image-img">
+                      <img className="img-s" src={post.image_url} />
+                    </div>
+                  </div>
                 </div>
 
-                <div className="post-description">
-                  <p>
+                <div className="description-footer">
+                  <div>
+                    <div className="description">
                     {post.description}
-                  </p>
-                  {post.user_id === this.props.currentUser.id ?
-                    <button onClick={() => this.props.deletePost(post.id)} className='deleteButton'>
-                      Delete Post
-                    </button> : null}
+                    </div>
+                    <span></span>
+                  </div>
                 </div>
-              </div>) : <h1>Loading....</h1>}
+
+                <div className="empty-div"></div>
+
+                <div className="post-footer">
+                  <div className="post-stuff">
+                    <div className="post-icons">
+                      <button className="upvote-button">
+                      </button>
+                      <button className="downvote-button">
+                      </button>
+                      <button className="favorite-button">
+                      </button>
+
+                      <div className="post-actions-right">
+                        <span className="ellipses">...</span>
+                          <div className="post-actions-right-dropdown">
+                            <div className>
+                              {post.user_id === this.props.currentUser.id ?
+                                <div onClick={() => this.props.deletePost(post.id)} className='deleteButton'>
+                                  Delete Post
+                                </div> : null}
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="social-icons-right">
+                          <span className="social-icon icon-f">
+                            <a href="http://www.facebook.com/" />
+                          </span>
+                          <span className="social-icon icon-twt">
+                            <a href="https://twitter.com/" />
+                          </span>
+                          <span className="social-icon icon-pin">
+                            <a href="http://pinterest.com/" />
+                          </span>
+                          <span className="social-icon icon-red">
+                            <a href="http://www.reddit.com/" />
+                          </span>
+                        </div>
+                      </div>
+                      <div className="post-data-footer">
+                        <span className="post-stats">
+                          <span className="stats-points">7 points</span>
+                          <span className="stats-views">22 views</span>
+                        </span>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+            </div>
+
+          </div>
+            ) : <h1>Loading....</h1>}
       </div>
     );
   }
