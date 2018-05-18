c8e7751b509c65f7b73fd44b908238cc132b09a0
Alexander Keefe <akeefe09@yahoo.com>
2018-05-18 01:19:32 -0400

    Fix navbar style

--- frontend/components/navbar/links_right_nav.jsx
+++ frontend/components/navbar/links_right_nav.jsx
@@ -3,16 +3,17 @@ import { connect } from 'react-redux';
 import { Router, Route, Link} from 'react-router-dom';
 import { logout } from '../../actions/session_actions';
 import { openModal } from '../../actions/modal_actions';
+import FaCloudUpload from 'react-icons/lib/fa/cloud-upload';
 
 const RightNav = ({ loggedIn, logout, user, openModal }) => {
   if (!loggedIn) {
     return (
       <ul className='user-nav'>
-        <li className="navbar-link">
+        <li className="navbar-link sign-in-l">
           <Link to='/login' className='navlink-btn'> sign in </Link>
         </li>
 
-        <li className='navbar-link'>
+        <li className='navbar-link sign-up-l'>
           <Link to='/signup' className='navlink-btn'> sign up </Link>
         </li>
       </ul>
@@ -20,18 +21,20 @@ const RightNav = ({ loggedIn, logout, user, openModal }) => {
   } else {
     return (
       <ul className='user-nav'>
+
         <li className="upload-btn" onClick={openModal.bind(this, 'upload')}>
           <span className="icon-upload">
           </span>
-          <span className="upload-button-text">New post
+          <span className="upload-button-text">
+            <FaCloudUpload className="upload-fa"/>New post
           </span>
         </li>
 
-        <li className="navbar-link" onClick={ ()=> logout()}>
+        <li className="navbar-link logout-l" onClick={ ()=> logout()}>
           <Link to='/' className='navlink-btn'> Logout </Link>
         </li>
 
-        <li className="navbar-link">
+        <li className="navbar-link user-nav-name">
           <Link to={`/users/${user.id}`}
             className='navlink-btn'> {user.username} </Link>
         </li>
